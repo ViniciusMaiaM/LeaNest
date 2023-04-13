21a6a4792ed975fe0e3862de1dd3a0d26726231f
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-04-13 20:40:08 -0300

    feat(messages): add base messages controller

--- first_project/src/messages/messages.controller.ts
+++ first_project/src/messages/messages.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {
+    console.log('test');
+  }
+
+  @Post()
+  // Declaring that this method will receive data from the body
+  createMssages(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
+
+  @Get(':id')
+  // Declaring that a information will be extracted from the parameters of the requisition
+  getMessages(@Param('id') id: string) {
+    console.log(id);
+  }
+}
